e9d8947bd0fca48ae3c150c6fcddb53f463817f8
yuehoif <yuehoif@gmail.com>
2023-03-20 22:33:06 +0800

    feat: implement event detail page

--- src/pages/events/[eventId].tsx
+++ src/pages/events/[eventId].tsx
@@ -1,9 +1,33 @@
+import { useRouter } from "next/router";
+
+import EventSummary from "@/components/event-detail/event-summary";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventContent from "@/components/event-detail/event-content";
+
+import { getEventById } from "../../dummy-data";
+
 export default function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId as string;
+
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No Event found</p>;
+  }
+
   return (
     <>
-      <main>
-        <h1>Event Detail Page</h1>
-      </main>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
     </>
   );
 }
